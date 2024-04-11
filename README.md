bdc87cf3b5e4709681c24657b6d14cfafc6f5347
andrew <andrew@whopper.none>
2024-04-11 14:49:11 -0400

    linux conda environment setup, test script to verify install

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # rl-pacman
+
+To install environment on linux:
+```sh bin/create-conda-cnv-torch.sh```
+
+Activating environment:
+```conda activate env/```
---
+++ bin/create-conda-env-torch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash --login
+
+set -e
+
+export ENV_PREFIX=$PWD/env
+
+mkdir -p $ENV_PREFIX
+
+eval "$(conda shell.bash hook)"
+conda env create --prefix $ENV_PREFIX --file environment-torch.yml --force
+conda activate $ENV_PREFIX
+
+echo "------------------------------"
+echo "VERIFYING PYTORCH INSTALLATION"
+echo "------------------------------"
+python3 -c "import torch;x=torch.rand(5, 3);print(x);print(torch.cuda.is_available())"
